a8a3072afc33d94988a107834fb67ce6700095b4
Jim Barritt <jim@planet-ix.com>
2010-10-18 00:22:46 +0100

    Added clojure tests and example

---
+++ encapsulation-clojure/src/main/clojure/jimbarritt/encapsulation/level_0/galleon.clj
@@ -0,0 +1,5 @@
+(ns jimbarritt.encapsulation.level_0.galleon)
+
+(def galleon-total-points-red-cards 4)
+(def galleon-total-points-black-cards 1)
+
---
+++ encapsulation-clojure/src/test/clojure/jimbarritt/encapsulation/level_0/no_encapsulation_tests.clj
@@ -0,0 +1,28 @@
+(ns jimbarritt.encapsulation.level_0.no-encapsulation-tests)
+
+(use 'clojure.contrib.test-is)
+(use 'jimbarritt.encapsulation.level_0.galleon)
+
+
+(deftest calculates-fighting-strength
+  (println "We can refer to the global variables even though they are defined in a different namespace to us:")
+  (println (format "Number of red points %d" galleon-total-points-red-cards))
+  (println (format "Number of black points %d" galleon-total-points-black-cards))
+
+  (is (= galleon-total-points-red-cards 4))
+  (is (= galleon-total-points-black-cards 1))
+
+  (def fighting-strength (- galleon-total-points-red-cards galleon-total-points-black-cards))
+
+  (is (= fighting-strength 3))
+)
+
+(deftest cannot-refer-to-local-variable
+  (println "The variable 'fighting-strength should be undefined")
+  (is (thrown? IllegalStateException (
+    (. System/out (println (format "Fighting Strength %d" fighting-strength)))
+  )))  
+)
+
+
+(run-tests)
