5b4a29171a07f3cd82691e280e44ed771ca637f3
Jim Barritt <jim@planet-ix.com>
2010-10-18 23:16:55 +0100

    Ignoring global variables and have added basic test of globals

--- encapsulation-javascript/src/test/javascript/level_0/no_encapsulation_test.js
+++ encapsulation-javascript/src/test/javascript/level_0/no_encapsulation_test.js
@@ -3,12 +3,14 @@ eval(loadFile("src/main/javascript/level_0/galleon.js"));
 
 testCases(test,
 
-    function setUp() {
-        var test = 4;
-    },
 
-    function canMakeAnAssertion() {
-        assert.that("foo", eq("bar"));
+    function canCalculateFightingStrength() {
+        
+        var fightingStrength = galleon_red_card_points - galleon_black_card_points;
+
+        assert.that(galleon_black_card_points, eq(1));
+        assert.that(galleon_red_card_points, eq(4));
+        assert.that(fightingStrength, eq(3));
     }
 
 );
