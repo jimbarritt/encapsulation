752c02976b3b570e9a3a182dc68b77ac78be91f2
Jim Barritt <jim@planet-ix.com>
2010-11-16 22:38:29 +0000

    Code formatting

--- encapsulation-java/src/test/java/com/jimbarritt/encapsulation/point_0/PointTest.java
+++ encapsulation-java/src/test/java/com/jimbarritt/encapsulation/point_0/PointTest.java
@@ -2,9 +2,8 @@ package com.jimbarritt.encapsulation.point_0;
 
 import org.junit.*;
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
-import static junit.framework.Assert.assertEquals;
+import static java.lang.Math.*;
+import static junit.framework.Assert.*;
 
 public class PointTest {
 
@@ -12,13 +11,13 @@ public class PointTest {
     public void calculates_the_distance_between_two_points() {
         Point p1 = new Point();
         p1.x = 10;
-        p1.y=20;
+        p1.y = 20;
 
         Point p2 = new Point();
         p2.x = 20;
         p2.y = 30;
 
-        double expectedDistance = sqrt(pow(20-10, 2) + pow(30-20, 2));
+        double expectedDistance = sqrt(pow(20 - 10, 2) + pow(30 - 20, 2));
 
         Plane plane = new Plane();
         double actualDistance = plane.distanceBetween(p1, p2);
