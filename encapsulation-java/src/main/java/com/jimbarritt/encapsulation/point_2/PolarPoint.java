b10b3dee2d8859f4b485fd0955e5a87897496412
Jim Barritt <jim@planet-ix.com>
2010-11-16 00:07:05 +0000

    Adding polar point

--- encapsulation-java/src/main/java/com/jimbarritt/encapsulation/point_2/PolarPoint.java
+++ encapsulation-java/src/main/java/com/jimbarritt/encapsulation/point_2/PolarPoint.java
@@ -1,7 +1,15 @@
 package com.jimbarritt.encapsulation.point_2;
 
 public class PolarPoint implements Point {
-    
+
+    final double theta;
+    final double r;
+
+    public PolarPoint(double theta, double r) {
+        this.theta = theta;
+        this.r = r;
+    }
+
     public double distanceTo(Point other) {
         return 0;
     }
