3799b4c1cbfe331360f0144d9ba637c729094ed4
Jim Barritt <jim@planet-ix.com>
2010-11-21 17:32:24 +0000

    changing comment

--- encapsulation-java/src/main/java/com/jimbarritt/encapsulation/point_0/Plane.java
+++ encapsulation-java/src/main/java/com/jimbarritt/encapsulation/point_0/Plane.java
@@ -1,12 +1,11 @@
 package com.jimbarritt.encapsulation.point_0;
 
-import static java.lang.Math.pow;
-import static java.lang.Math.sqrt;
+import static java.lang.Math.*;
 
 public class Plane {
 
     /**
-     * Uses pythagoras to calculate the distance, h^2 = a^2 + b^2
+     * Uses pythagoras' equation to calculate the distance, h^2 = a^2 + b^2
      */
     public double distanceBetween(Point p1, Point p2) {
         int a = p2.x - p1.x;
